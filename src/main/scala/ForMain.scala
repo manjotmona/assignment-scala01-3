43c4bd5d26b6fb0e5b82b82708e9f7f99ac6050c
Manjot Kaur <manjotmona@gmail.com>
2018-01-10 22:04:07 +0530

    Some Changes

--- src/main/scala/ForMain.scala
+++ src/main/scala/ForMain.scala
@@ -15,10 +15,40 @@ object ForMain extends App{
 
 
   var result = obj.produceSum(list,list)
-  print("Sum of 2 lists:" + result+ "\n")
+  print("Sum of 2 lists: " + result + "\n")
 
-print("")
-  print(obj.numberOfElements(list))
+  print("Number of Elements: ")
+  print(obj.numberOfElements(list) + "\n")
+
+  print("Kth Element of list: ")
+  print(obj.nthElementOfList(three,list) + "\n")
+
+  print("Reverse List: ")
+  obj.reverseList(list)
+  print("\n")
 
 
+  print("Double Elements of List: ")
+  print(obj.doubleElements(list) + "\n")
+
+  print("Fibonacci Series: ")
+  obj.fibonacciForLoop(six)
+
+  print("\n")
+  print("Area of square :Input parameters are sides\n")
+  print(obj.area("square",four,four,(a,b)=>a * b))
+
+  print("\n")
+  print("Area of rectangle: Input parameters are length and breadth\n")
+  print(obj.area("rectangle",four,five,(a,b)=>a * b))
+  print("\n")
+
+  print("Area of rhombus: Input parameter are diagonals\n")
+  print(obj.area("rhombus",four,four,(a,b)=>(a * b)/2))
+
+  print("\nArea of parallelogram: Input parameters are base and height\n")
+  print(obj.area("parallelogram",four,four,(a,b)=>a * b))
+
+  print("\nArea of square\n")
+  print(obj.area("square",one,four,(a,b)=>a * b))
 }
--- src/main/scala/Operations.scala
+++ src/main/scala/Operations.scala
@@ -5,7 +5,7 @@ import scala.collection.mutable.ListBuffer
  */
 class Operations {
 
-  def sum(first: Int, second: Int, f: (Int,Int) => Int) = {
+  def sum(first: Int, second: Int, f: (Int,Int) => Int): Int = {
     f(first, second)
   }
 
@@ -22,25 +22,30 @@ class Operations {
 
       }
 
-  def fibonacci(n: Int): Int = {
-    def fibonacciRecursive(n: Int, a: Int, b: Int): Int = n match {
-      case 0 => a
-      case _ => fibonacciRecursive(n-1,b,a + b)
+  def fibonacci(num: Int): Int = {
+    def fibonacciRecursive(num: Int, term1: Int, term2: Int): Int = num match {
+      case 0 => term1
+      case _ => fibonacciRecursive(num-1,term2,term1 + term2)
     }
-    return fibonacciRecursive(n, 0 , 1)
+     fibonacciRecursive(num, 0 , 1)
+  }
+
+  def fibonacciForLoop(num: Int): Unit ={
+    for(x <- 0 until num)
+      print(fibonacci(x))
   }
 
 
   def area(shape: String, first: Int, second: Int, f:(Int,Int) => Int): String = {
     shape match{
-      case "square" => "Area of" + shape + "is " + f(first,second)
-      case "rectangle" => "Area of" + shape + "is " + f(first,second)
-      case "rhombus" => "Area of" + shape + "is " + f(first,second)
-      case "parallelogram" => "Area of" + shape + "is " + f(first,second)
+      case "square" if(first == second ) => "Area of " + shape + " is " + f(first,second)
+      case "rectangle" => "Area of " + shape + " is " + f(first,second)
+      case "rhombus" => "Area of " + shape + " is " + f(first,second)
+      case "parallelogram" => "Area of " + shape + " is " + f(first,second)
       case _ => "We can't calculate area of this shape !!"
 
     }
-    "Area of" + shape + "is " + f(first,second)
+
   }
 
 
@@ -53,11 +58,9 @@ class Operations {
 
   def nthElementOfList(position: Int, list: List[Int]): Int = {
     list match {
-      case head :: tail if (position == 0) => head
-      case head :: tail if (position >0 ) => nthElementOfList(position-1,tail)
-
+      case head :: tail if (position == 1) => head
+      case head :: tail if (position >1 ) => nthElementOfList(position-1,tail)
     }
-
   }
 
   def numberOfElements(list: List[Int]): Int = {
@@ -68,16 +71,11 @@ class Operations {
 
   }
 
-  def reverseList(list: List[Int]): Unit ={
-    for(x <- 0 to list.length-1){
-    print(list(list.length-x-1))
+
+  def reverseList(list: List[Int]): Unit = {
+    for (x <- 0 to list.length - 1) {
+      print(list(list.length - x - 1))
     }
-
-
   }
-
-
-
-
 
 }
